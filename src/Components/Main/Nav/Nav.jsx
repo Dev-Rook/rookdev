0c8948bc37adf498df60608b3ed09c9ae148265b
Dev Rook <110010478+Dev-Rook@users.noreply.github.com>
2022-09-28 23:58:24 -0500

    Outline Menu

--- src/Components/Main/Nav/Nav.jsx
+++ src/Components/Main/Nav/Nav.jsx
@@ -8,11 +8,19 @@ const Nav = () => {
     setMenu((prev) => !prev);
   };
 
+  const hideMenu = () => {
+    setMenu((prev) => !prev);
+  };
+
   return (
     <div className={Styles.Nav}>
       <div className={Styles.Left_Side}>
         <div className={Styles.Logo_Container}>
-          <img src={require("../../../Assests/Images/Logo.png")} className={Styles.Logo} alt="" />
+          <img
+            src={require("../../../Assests/Images/Logo.png")}
+            className={Styles.Logo}
+            alt=""
+          />
         </div>
 
         <h3 className={Styles.Brand}>Dev Rook</h3>
@@ -41,13 +49,50 @@ const Nav = () => {
 
       {/* Mobile Menu Start  */}
 
-      <div className={`${Styles.Mobile_Menu} ${menu ? Styles.ShowMenu : ""}`}>
-      <img
-          onClick={showMenu}
-          src={require("../../../Assests/Icons/Close-Icon.png")}
-          alt=""
-          className={Styles.Close_Icon}
-        />
+      <div
+        onClick={showMenu}
+        className={`${Styles.Mobile_Menu} ${menu ? Styles.ShowMenu : ""}`}
+      >
+        <div className={Styles.Content_Container}>
+          <div className={Styles.Header}>
+            <div className={Styles.Logo_Container}>
+              <img
+                src={require("../../../Assests/Images/Logo.png")}
+                className={Styles.Logo}
+                alt=""
+              />
+            </div>
+
+            <img
+              onClick={hideMenu}
+              src={require("../../../Assests/Icons/Close-Icon.png")}
+              alt=""
+              className={Styles.Close_Icon}
+            />
+          </div>
+          <ul className={Styles.Mobile_Navlink_Container}>
+            <li className={Styles.Menu_NavItem}>
+              <a href="#" className={Styles.Menu_Navlink}>
+                Home
+              </a>
+            </li>
+            <li className={Styles.Menu_NavItem}>
+              <a href="#" className={Styles.Menu_Navlink}>
+                Portfolio
+              </a>
+            </li>
+            <li className={Styles.Menu_NavItem}>
+              <a href="#" className={Styles.Menu_Navlink}>
+                Contact
+              </a>
+            </li>
+            <li className={Styles.Menu_NavItem}>
+              <a href="#" className={Styles.Menu_Navlink}>
+                Comunity
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
 
       {/* Mobile Menu End */}
