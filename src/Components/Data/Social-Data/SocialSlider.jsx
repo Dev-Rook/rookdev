b5c4426922aac3f9f80746e9a687b5218a8a271d
Dev Rook <110010478+Dev-Rook@users.noreply.github.com>
2022-10-04 14:30:49 -0500

    Stuff

--- src/Components/Data/Social-Data/SocialSlider.jsx
+++ src/Components/Data/Social-Data/SocialSlider.jsx
@@ -1,6 +1,15 @@
 import React, { useState } from "react";
 import Styles from "../../../Styles/Swiper-Styles/Social/SocialSlider.module.css";
 
+// Material UI Imports Start
+
+import CodeIcon from "@mui/icons-material/Code";
+import HttpIcon from "@mui/icons-material/Http";
+// import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+
+// Material UI Imports End
+
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, FreeMode, Autoplay, Mousewheel } from "swiper";
@@ -20,66 +29,24 @@ const SocialSlider = () => {
 
   return (
     <div className={Styles.Social_Container}>
-      <h3 className={Styles.Slider_Title}>Social</h3>
+      <h3 className={Styles.Section_Title}>Social</h3>
       <h2 className={Styles.Section_Description}>Find me on these platforms</h2>
-      <Swiper
-        speed={800}
-        modules={[Navigation, FreeMode, Mousewheel, Autoplay]}
-        autoplay={{
-          delay: 2000,
-          disableOnInteraction: false,
-        }}
-        grabCursor={true}
-        loop
-        pagination={{
-          dynamicBullets: true,
-        }}
-        mousewheel={true}
-        className={Styles.Swiper}
-        breakpoints={{
-          360: {
-            width: 360,
-            slidesPerView: 1,
-            spaceBetween: 10,
-          },
-          380: {
-            width: 380,
-            slidesPerView: 1,
-            spaceBetween: 10,
-          },
-          480: {
-            width: 480,
-            slidesPerView: 2,
-            spaceBetween: 10,
-          },
-          768: {
-            width: 768,
-            slidesPerView: 3,
-            spaceBetween: 10,
-          },
-          1920: {
-            width: 1920,
-            slidesPerView: 5,
-            spaceBetween: 0,
-          },
-        }}
-      >
-        {cardData.map((slide) => {
-          const { id, Name, Image, Description, Stacks, Links } = slide;
+      {cardData.map((Card) => {
+        const { id, Image, Name } = Card;
 
-          return (
-            <SwiperSlide className={Styles.Slide}>
-              <div className={Styles.Social_Card_Container} key={id}>
-                <div className={Styles.Image_Container}>
-                  <img src={Image} alt="" className={Styles.Experience_Image} />
-                </div>
-
-                <h3 className={Styles.Name}>{Name}</h3>
+        return (
+          <div className={Styles.Card} key={id}>
+            <div className={Styles.Imgae__Title_Container}>
+              <div className={Styles.Image_Container}>
+                <img src={Image} alt="" className={Styles.Social_Image} />
               </div>
-            </SwiperSlide>
-          );
-        })}
-      </Swiper>
+              <h3 className={Styles.Name}>{Name}</h3>
+            </div>
+
+            <div className={Styles.Social_Button}>View</div>
+          </div>
+        );
+      })}
     </div>
   );
 };
