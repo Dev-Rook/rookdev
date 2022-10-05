04b7ee1fe12651f60e4964797a7211d4c50ca41b
Dev Rook <110010478+Dev-Rook@users.noreply.github.com>
2022-10-04 20:05:16 -0500

    Pages

--- src/Views/Dev-Logs/DevLogs.jsx
+++ src/Views/Dev-Logs/DevLogs.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from "react";
+import Styles from "../../Styles/Page-Styles/Dev-Logs/DevLogs.module.css";
+
+import { Link } from "react-router-dom";
 
 const DevLogs = () => {
   return (
-    <div>DevLogs</div>
-  )
-}
+    <div className={Styles.DevLogs}>
+      <h3 className={Styles.Page_Title}>Coming Soon</h3>
 
-export default DevLogs
+      <div className={Styles.Contact_Implore_Box}>
+        <h3 className={Styles.Section_Title}>Get In Touch?</h3>
+        <h3 className={Styles.Section_Description}>
+          Join my Discord or shoot me a message VIA contact form.
+        </h3>
+        <div className={Styles.Button_Container}>
+          <a
+            target={"_blank"}
+            rel={"noreferrer"}
+            href="https://discord.gg/ce7mtCbgmG"
+          >
+            <div
+              className={`${Styles.Contact_Button} ${Styles.Discord__Button}`}
+            >
+              Discord
+            </div>
+          </a>
+          <Link to={"Contact"}>
+            <div
+              className={`${Styles.Contact_Button} ${Styles.Contact__Button}`}
+            >
+              Contact
+            </div>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DevLogs;
