797f23c1ea2acc5e31e1327010130a19e9bc7148
Dev Rook <110010478+Dev-Rook@users.noreply.github.com>
2022-10-04 19:32:33 -0500

    Pages Start

--- src/Views/Contact/Contact.jsx
+++ src/Views/Contact/Contact.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from "react";
+import Styles from "../../Styles/Page-Styles/Contact/Contact.module.css";
+
+import { Link } from "react-router-dom";
 
 const Contact = () => {
   return (
-    <div>Contact</div>
-  )
-}
+    <div className={Styles.Contact}>
+      <h3 className={Styles.Page_Title}>Contact</h3>
 
-export default Contact
+      <div className={Styles.Contact_Implore_Box}>
+        <h3 className={Styles.Section_Description}>Join my Discord</h3>
+        <div className={Styles.Button_Container}>
+          <a
+            target={"_blank"}
+            rel={"noreferrer"}
+            href="https://discord.gg/ce7mtCbgmG"
+          >
+            <div
+              className={`${Styles.Contact_Button} ${Styles.Discord__Button}`}
+            >
+              Discord
+            </div>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
--- src/Views/Portfolio/Portfolio.jsx
+++ src/Views/Portfolio/Portfolio.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React from "react";
+import Styles from "../../Styles/Page-Styles/Portfolio/Portfolio.module.css";
+
+import { Link } from "react-router-dom";
 
 const Portfolio = () => {
   return (
-    <div>Portfolio</div>
-  )
-}
+    <div className={Styles.Portfolio}>
+      <h3 className={Styles.Page_Title}>Portfolio</h3>
 
-export default Portfolio
+
+
+      <div className={Styles.Contact_Implore_Box}>
+        <h3 className={Styles.Section_Title}>Get In Touch?</h3>
+        <h3 className={Styles.Section_Description}>
+          Join my Discord or shoot me a message VIA contact form.
+        </h3>
+        <div className={Styles.Button_Container}>
+          <a
+            target={"_blank"}
+            rel={"noreferrer"}
+            href="https://discord.gg/ce7mtCbgmG"
+          >
+            <div
+              className={`${Styles.Contact_Button} ${Styles.Discord__Button}`}
+            >
+              Discord
+            </div>
+          </a>
+          <Link to={"Contact"}>
+            <div
+              className={`${Styles.Contact_Button} ${Styles.Contact__Button}`}
+            >
+              Contact
+            </div>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Portfolio;
