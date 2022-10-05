34b238566b40581acd5fa96729c24f213e6a786c
Dev Rook <110010478+Dev-Rook@users.noreply.github.com>
2022-10-04 19:17:19 -0500

    Footer update

--- src/Components/Main/Footer/Footer.jsx
+++ src/Components/Main/Footer/Footer.jsx
@@ -1,14 +1,54 @@
-import React from 'react'
-import Styles from "../../../Styles/Component-Styles/Footer/Footer.module.css"
+import React from "react";
+import Styles from "../../../Styles/Component-Styles/Footer/Footer.module.css";
 
+import { Link } from "react-router-dom";
+
+import LogoDevIcon from "@mui/icons-material/LogoDev";
 
 const Footer = () => {
   return (
     <div className={Styles.Footer}>
-        {/* ※ End Of Website ※ */}
-        © 2022 Developer Rook.
-    </div>
-  )
-}
+      <div className={Styles.Footer_Content}>
+        <div className={Styles.Logo__Brand}>
+          <Link to={"/"}>
+            <div className={Styles.Logo_Container}>
+              <img
+                src={require("../../../Assests/Images/Logo.png")}
+                className={Styles.Logo}
+                alt=""
+              />
+            </div>
+          </Link>
 
-export default Footer
+          <h3 className={Styles.Brand}>Dev Rook</h3>
+        </div>
+
+        <ul className={Styles.Navlink_Container}>
+          <li className={Styles.NavItem}>
+            <Link to={"/"} className={Styles.Navlink}>
+              Home
+            </Link>
+          </li>
+          <li className={Styles.NavItem}>
+            <Link to={"Portfolio"} className={Styles.Navlink}>
+              Portfolio
+            </Link>
+          </li>
+          <li className={Styles.NavItem}>
+            <Link to={"Contact"} className={Styles.Navlink}>
+              Contact
+            </Link>
+          </li>
+          <li className={Styles.NavItem}>
+            <Link to={"DevLogs"} className={Styles.Navlink}>
+              <LogoDevIcon className={Styles.NavIcon} />
+            </Link>
+          </li>
+        </ul>
+      </div>
+      {/* ※ End Of Website ※ */}© 2022 Developer Rook.
+    </div>
+  );
+};
+
+export default Footer;
