b5d1748b710462a800be634509b85f0bd3382b68
Dev Rook <110010478+Dev-Rook@users.noreply.github.com>
2022-10-16 12:06:11 -0500

    Edited Pagination Card width for larger descriptions

--- src/Components/Data/Project-Data/Featured-Projects/FeaturedData.jsx
+++ src/Components/Data/Project-Data/Featured-Projects/FeaturedData.jsx
@@ -1,6 +1,26 @@
 const FeaturedData = [
   {
     id: 1,
+    Image:
+      "https://github.com/Dev-Rook/rookdev/blob/master/src/Assests/Images/Website-Logos/Fake%20Mer%20Website%20Thumbnail%20for%20Portfolio%20Project.png?raw=true",
+    Title: "Fake Product Website",
+    Category: "Website",
+    Description:
+      "This website incorporates a fake product API to display the product data onto sliders which dynamically routes a user to the specific product details if clicked.",
+    Stacks: {
+      Stack1: "React",
+      Stack2: "MUI",
+      Stack3: "Axios",
+      Stack4: "Swiper JS",
+      Stack5: "Scss",
+    },
+    Links: {
+      Code: "https://github.com/Dev-Rook/merch",
+      Demo: "https://fakemerch.vercel.app/",
+    },
+  },
+  {
+    id: 2,
     Image: "https://avatars.githubusercontent.com/u/110010478?v=4",
     Title: "Weather App",
     Category: "",
@@ -19,7 +39,7 @@ const FeaturedData = [
     },
   },
   {
-    id: 2,
+    id: 3,
     Image: "",
     Title: "Learn J22",
     Category: "",
@@ -35,25 +55,6 @@ const FeaturedData = [
     Links: {
       Code: "https://github.com/Dev-Rook/learn-j22",
       Demo: "https://learnj22.vercel.app/",
-    },
-  },
-  {
-    id: 3,
-    Image: "",
-    Title: "Website Title",
-    Category: "",
-    Description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, maxime!",
-    Stacks: {
-      Stack1: "React",
-      Stack2: "MUI",
-      Stack3: "Framer Motion",
-      Stack4: "AOS",
-      Stack5: "Scss",
-    },
-    Links: {
-      Code: "",
-      Demo: "",
     },
   },
   {
